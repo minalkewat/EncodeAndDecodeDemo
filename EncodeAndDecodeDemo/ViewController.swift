f86af0bfbc778f7575c2b81a7a17e9591543d0e5
minalkewat <minalkewat@gmail.com>
2019-06-05 13:31:29 +0530

    Initial Commit

---
+++ EncodeAndDecodeDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EncodeAndDecodeDemo
+//
+//  Created by Meenal Kewat on 6/5/19.
+//  Copyright © 2019 Meenal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
